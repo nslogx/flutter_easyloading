7d312044757281353d57904db395a5c50e90a8ed
huangjianke <huangjianke@vip.163.com>
2020-09-28 14:35:30 +0800

    update version 2.0.0

--- lib/src/easy_loading.dart
+++ lib/src/easy_loading.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import './widgets/container.dart';
 import './widgets/progress.dart';
