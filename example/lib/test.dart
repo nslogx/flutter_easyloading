4bc66058dee0fb45e5e15cc22e1aab5200ba55a8
huangjianke <huangjianke@vip.163.com>
2020-09-30 10:16:33 +0800

    update example

--- example/lib/test.dart
+++ example/lib/test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:dio/dio.dart';
 
 class TestPage extends StatefulWidget {
   @override
@@ -10,13 +11,25 @@ class _TestPageState extends State<TestPage> {
   @override
   void initState() {
     super.initState();
-    EasyLoading.show();
+    // EasyLoading.show();
   }
 
   @override
   void deactivate() {
     EasyLoading.dismiss();
     super.deactivate();
+  }
+
+  void loadData() async {
+    try {
+      EasyLoading.show();
+      Response response = await Dio().get('https://github.com');
+      print(response);
+      EasyLoading.dismiss();
+    } catch (e) {
+      EasyLoading.showError(e.toString());
+      print(e);
+    }
   }
 
   @override
@@ -28,12 +41,13 @@ class _TestPageState extends State<TestPage> {
       body: Center(
         child: FlatButton(
           textColor: Colors.blue,
-          child: Text('async show'),
-          onPressed: () async {
-            await Future.delayed(Duration(seconds: 2));
-            EasyLoading.show(status: 'loading...');
-            await Future.delayed(Duration(seconds: 5));
-            EasyLoading.dismiss();
+          child: Text('loadData'),
+          onPressed: () {
+            loadData();
+            // await Future.delayed(Duration(seconds: 2));
+            // EasyLoading.show(status: 'loading...');
+            // await Future.delayed(Duration(seconds: 5));
+            // EasyLoading.dismiss();
           },
         ),
       ),
