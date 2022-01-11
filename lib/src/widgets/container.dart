ee0ef2f22c4d5eb076cdddcf1f8a39863d92df87
flyzero <mzero126@126.com>
2022-01-11 10:20:21 +0800

    fix toastPosition bug

--- lib/src/widgets/container.dart
+++ lib/src/widgets/container.dart
@@ -73,9 +73,8 @@ class EasyLoadingContainerState extends State<EasyLoadingContainer>
     super.initState();
     if (!mounted) return;
     _status = widget.status;
-    _alignment = (widget.indicator == null && widget.status?.isNotEmpty == true)
-        ? EasyLoadingTheme.alignment(widget.toastPosition)
-        : AlignmentDirectional.center;
+    _alignment = EasyLoadingTheme.alignment(
+        widget.toastPosition ?? EasyLoadingTheme.toastPosition);
     _dismissOnTap =
         widget.dismissOnTap ?? (EasyLoadingTheme.dismissOnTap ?? false);
     _ignoring =
